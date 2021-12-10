26cbdbf4c2fc00c15799804b9993d17cce3fed42
malav3005 <95560078+malav3005@users.noreply.github.com>
2021-12-10 16:30:39 +0530

    Update SpeakGoodBye.js

--- module4/js/SpeakGoodBye.js
+++ module4/js/SpeakGoodBye.js
@@ -1,7 +1,7 @@
 (function(window) {
 	var speakWord = "Good Bye";
 	var byeSpeaker = function (name) {
-  		console.log(speakWord + " " + name);
+  		console.log("J "+speakWord + " " + name);
 	}
 	
 	window.byeSpeaker = byeSpeaker;
